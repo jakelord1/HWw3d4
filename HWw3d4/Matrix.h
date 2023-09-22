add5e94417535b15d11939d91c774202d88d2930
jakelord1 <jakelord1.vo@gmail.com>
2023-09-22 14:09:24 +0300

    Добавьте файлы проекта.

---
+++ HWw3d4/Matrix.h
@@ -0,0 +1,17 @@
+#pragma once
+template<class A = int>
+class Matrix
+{
+	A** arr;
+	int rows;
+	int cols;
+public:
+	Matrix() = default;
+	~Matrix() {
+		for (size_t i = 0; i < length; i++)
+		{
+
+		}
+	}
+};
+
